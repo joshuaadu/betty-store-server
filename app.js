4ad1072822d698f73c42682529c76716c6b05c68
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-09-05 16:46:18 +0000

    serve files statically

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+const rootDir = require("./util/path");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
