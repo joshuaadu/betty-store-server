14295f41b04aabbc058e075be523f3443ed58629
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-10-04 17:48:51 +0000

    set up server with nestjs

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.use("/products", (req, res, next) => {
-  res.send({ products: ["iphone", "samsung"] });
-});
-
-app.listen(3001);
